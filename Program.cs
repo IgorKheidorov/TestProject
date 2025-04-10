75818c0c23f8b19604b080be0bee52722459ee72
IgorKheidorov <igorkheidorov@coherentsolutions.com>
2025-04-10 14:17:18 +0300

    Program is added

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Program
+{
+    public class Program 
+    {
+        public static void SwapMinMax(int[] array)
+        {
+            if (array == null || array.Length == 0) return;
+
+            int minIndex = 0, maxIndex = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < array[minIndex])
+                    minIndex = i;
+                if (array[i] > array[maxIndex])
+                    maxIndex = i;
+            }
+
+            int temp = array[minIndex];
+            array[minIndex] = array[maxIndex];
+            array[maxIndex] = temp;
+        }
+
+        public static void Main()
+        {
+            int[] data = [1, 2, 5, -10, 0];
+            SwapMinMax(data);        
+        }
+
+    }
+
+}
